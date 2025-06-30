b445a450eec2e9e35b44255fcf68911d1024ce69
Traam0 <younesMRX@outlook.com>
2025-06-30 12:48:32 +0100

    analytics2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,34 +4,46 @@ import starlight from '@astrojs/starlight';
 import catppuccin from "@catppuccin/starlight";
 
 import tailwindcss from '@tailwindcss/vite';
+// import vercel from '@astrojs/';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [starlight({
-        plugins: [catppuccin()],
-        customCss: ["./src/styles/global.css"],
-        title: 'UniR API',
-        social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/Traam0'}],
-        sidebar: [{
-            label: 'Getting Started',
-            items: [{label: "Overview", slug: "guides/overview"}, // Each item here is one entry in the navigation menu.
-                {label: 'Authentication', slug: 'guides/authentication'},],
-        }, {
-            label: "API",
-
-            items: [
+    integrations: [
+        starlight({
+            head: [
                 {
-                    label: "Academics",
+                    tag: 'script',
+                    attrs: { type: 'module', src: '@vercel/analytics', async: true },
+                },
+            ],
+            plugins: [catppuccin()],
+            customCss: ["./src/styles/global.css"],
+            title: 'UniR API',
+            social: [{icon: 'github', label: 'GitHub', href: 'https://github.com/Traam0'}],
+            sidebar: [
+                {
+                    label: 'Getting Started',
+                    items: [{label: "Overview", slug: "guides/overview"}, // Each item here is one entry in the navigation menu.
+                        {label: 'Authentication', slug: 'guides/authentication'},],
+                },
+                {
+                    label: "API",
                     items: [
-                        {label: "Academic Programs", slug: "api/academics/programs",},
-                        {label: "Branches", slug: "api/academics/branches",}
+                        {
+                            label: "Academics",
+                            items: [
+                                {label: "Academic Programs", slug: "api/academics/programs",},
+                                {label: "Branches", slug: "api/academics/branches",}
+                            ]
+                        }
                     ]
-                }
-            ]
-        }, {
-            label: 'Reference', autogenerate: {directory: 'reference'},
-        },],
-    }),],
+                },
+                {
+                    label: 'Reference', autogenerate: {directory: 'reference'},
+                },
+            ],
+        }),
+    ],
     vite: {
         plugins: [tailwindcss()],
     },
